eeb4b0303daaf7e9d67ab92a988aa1b628e286d6
anas deyra <anasdoura607@gmail.com>
2022-06-10 21:04:25 +0100

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Work from "./components/Work";
+import Work from "../components/Work";
 
 import { useScrollIntoView } from "@mantine/hooks";
 
-import Features from "./components/Features.js";
-import Hero from "./components/Hero";
-import Technologies from "./components/Technologies";
-import { BottomCurves } from "./components/Curves";
+import Features from "../components/Features.js";
+import Hero from "../components/Hero";
+import Technologies from "../components/Technologies";
 
 export default function Home() {
   const { scrollIntoView, targetRef } = useScrollIntoView();
